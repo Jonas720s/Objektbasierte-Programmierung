7c3a33a7761d5f5da3bdf225f0d72bd7c5ab3a32
Jonas Gemeinder <jonas.gemeinder@bluewin.ch>
2023-08-22 08:37:19 +0200

    Add ShowResult not finished

---
+++ Objektbasierte Programmierung/Aufgabe 1-9/Aufgabe 1-9/MyMath.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_1_9
+{
+    internal class MyMath
+    {
+        public static int Calc_ggT(int a, int b)
+        {
+            int z = 0;
+            while (b != 0)
+            {
+                z = a % b;
+                a = b;
+                b = z;
+            }
+            return a;
+        }
+
+        public static int Calc_kgV(int a, int b)
+        {
+            return a * b / Calc_ggT(a, b);
+        }
+    }
+}
---
+++ Objektbasierte Programmierung/Aufgabe 1-9/Aufgabe 1-9/Program.cs
@@ -0,0 +1,33 @@
+﻿using Aufgabe_1_9;
+
+namespace Aufgabe_Funktionen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12, b = 16;
+            Console.WriteLine(MyMath.Calc_ggT(a, b));
+            Console.WriteLine(MyMath.Calc_kgV(a, b));
+            Console.WriteLine($"Eingegebene Zahl: {ReadInt()}");
+        }
+
+        static int ReadInt()
+        {
+            int i;
+            do
+            {
+                Console.WriteLine("Bitte Zahl eingeben");
+                string s = Console.ReadLine();
+                int.TryParse(s, out i);
+
+            } while (i <= 0);
+            Console.WriteLine("danke");
+            return i;
+        }
+        public static void ShowResult(string[] args)
+        {
+            Console.WriteLine($"ggT von: {a} und {b} ist: {z}");
+        }
+    }
+}
