3168d2a8b3bed5c29132fca3f3ebdb4df99e3e27
Jonas Gemeinder <jonas.gemeinder@bluewin.ch>
2023-08-22 17:29:23 +0200

    solve task 1-9

--- Objektbasierte Programmierung/Aufgabe 1-9/Aufgabe 1-9/MyMath.cs
+++ Objektbasierte Programmierung/Aufgabe 1-9/Aufgabe 1-9/MyMath.cs
@@ -24,5 +24,59 @@ namespace Aufgabe_1_9
         {
             return a * b / Calc_ggT(a, b);
         }
+        public static void Swap(ref int a, ref int b)
+        {
+            int z = b;
+            b = a;
+            a = z;
+
+        }
+        public static int Calc_ggT_r(int a, int b)
+        {
+            int y = 0;
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return y = a % b;
+                b = y;
+            }
+        }
+        public static double ArithmeticMean(double[] values)
+        {
+            double sum = 0;
+            foreach (int v in values) {
+            sum += v;}
+            return sum / values.Length;
+        }
+        public static double SmallestValue(double[] values)
+        {
+            double numSmallest = values[0];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] < numSmallest)
+                {
+                    numSmallest = values[i];
+                }
+            }
+            return numSmallest;
+        }
+
+        public static double BiggestValue(double[] values)
+        {
+            double numBiggest = values[0];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] > numBiggest)
+                {
+                    numBiggest = values[i];
+                }
+            }
+            return numBiggest;
+        }
     }
 }
--- Objektbasierte Programmierung/Aufgabe 1-9/Aufgabe 1-9/Program.cs
+++ Objektbasierte Programmierung/Aufgabe 1-9/Aufgabe 1-9/Program.cs
@@ -4,12 +4,39 @@ namespace Aufgabe_Funktionen
 {
     internal class Program
     {
+        public static int ReadIntA()
+        {
+            int a;
+            do
+            {
+                Console.Write("geben sie eine Zahl ein: ");
+                string one = Console.ReadLine();
+                int.TryParse(one, out a);
+            } while (a <= 0);
+            Console.WriteLine("danke");
+            return a;
+        }
+        public static int ReadIntB()
+        {
+            int b;
+            do
+            {
+                Console.Write("geben sie eine Zahl ein: ");
+                string two = Console.ReadLine();
+                int.TryParse(two, out b);
+            } while (b <= 0);
+            Console.WriteLine("danke");
+            return b;
+        }
         static void Main(string[] args)
         {
-            int a = 12, b = 16;
-            Console.WriteLine(MyMath.Calc_ggT(a, b));
-            Console.WriteLine(MyMath.Calc_kgV(a, b));
-            Console.WriteLine($"Eingegebene Zahl: {ReadInt()}");
+            int a = ReadIntA();
+            int b = ReadIntB();
+            Console.WriteLine($"ggT von a und b: {MyMath.Calc_ggT_r(a, b)}");
+            Console.WriteLine($"kgV von a und b: {MyMath.Calc_kgV(a, b)}");
+            //Console.WriteLine($"Eingegebene Zahl: {ReadInt()}");
+            MyMath.Swap(ref a, ref b);
+            Console.WriteLine($"a: {a} b: {b}");
         }
 
         static int ReadInt()
@@ -24,10 +51,8 @@ namespace Aufgabe_Funktionen
             } while (i <= 0);
             Console.WriteLine("danke");
             return i;
+
         }
-        static void ShowResult(string[] args)
-        {
-            Console.WriteLine($"ggT von: {a} und {b} ist: {z}");
-        }
+
     }
 }
