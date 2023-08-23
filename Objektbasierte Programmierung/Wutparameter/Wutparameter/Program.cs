4337573a0871615978c9bd505388418e5c29cc27
Jonas Gemeinder <jonas.gemeinder@bluewin.ch>
2023-08-23 16:28:00 +0200

    gelöst mit fehler

--- Objektbasierte Programmierung/Wutparameter/Wutparameter/Program.cs
+++ Objektbasierte Programmierung/Wutparameter/Wutparameter/Program.cs
@@ -2,9 +2,68 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Main(string[] args, bool)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Wilkommen beim Wutparameter: ");
+
+            int p = ReadIntP();
+            string eg = ReadIntEG();
+            //int ev = ReadIntEV();
+            int a = ReadIntA();
+            Person person = new Person();
+            person.Provozieren(p);
+            person.EssenGeben(eg);
+            //person.EssenVerweigern();
+            person.ArbeitsauftragGeben(a);
+        }
+        public static int ReadIntP()
+        {
+            int p;
+            do
+            {
+                Console.Write("werden Sie provoziert? von 1-10: ");
+                string one = Console.ReadLine();
+                int.TryParse(one, out p);
+            } while (p <= 0);
+            Console.WriteLine("danke");
+            return p;
+        }
+        public static string ReadIntEG()
+        {
+            string eg;
+            do
+            {
+                Console.Write("Sind Sie satt?");
+                string two = Console.ReadLine();
+                two = two.ToLower();
+                
+            }
+            while (two == "yes", "no");
+        }
+        /*public static int ReadIntEV()
+        {
+            int ev;
+            do
+            {
+                Console.Write("Wann hast du das letzte mal kein essen bekommen? ");
+                string three = Console.ReadLine();
+                int.TryParse(three, out ev);
+            } while (ev <= 0);
+            Console.WriteLine("danke");
+            return ev;
+        }*/
+        public static int ReadIntA()
+        {
+            int a;
+            do
+            {
+                Console.Write("Wie viel arbeiten sie in letzter zeit von 1-10? ");
+                string four = Console.ReadLine();
+                int.TryParse(four, out a);
+            } while (a <= 0);
+            Console.WriteLine("danke");
+            return a;
         }
     }
 }
