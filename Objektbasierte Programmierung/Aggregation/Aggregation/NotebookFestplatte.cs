f8636cb6ad58dc6b2f3e82561868ac55e116b6a8
Jonas Gemeinder <jonas.gemeinder@bluewin.ch>
2023-08-23 16:44:16 +0200

    Aggregation Aufgabe 1

---
+++ Objektbasierte Programmierung/Aggregation/Aggregation/NotebookFestplatte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregation
+{
+    internal class NotebookFestplatte
+    {
+        {
+        public int FestplatteId { get; set; }
+        public string Name { get; set; }
+
+        //Aggregation zu Notebook
+        public Notebook Notebook { get; set; }
+        }
+    }
+}
