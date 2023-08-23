e502f1679c30adfa9cb856fbf37fec8a50362ffb
Jonas Gemeinder <jonas.gemeinder@bluewin.ch>
2023-08-23 16:48:09 +0200

    Komposition Aufgabe 1

---
+++ Objektbasierte Programmierung/Komposition/Komposition/Konto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komposition
+{
+    internal class Konto
+    {
+        public int KontoId { get; set; }
+        public string Name { get; set; }
+
+        //Aggregation zu Kunde
+        public Kunde Kunde { get; set; }
+    }
+}
---
+++ Objektbasierte Programmierung/Komposition/Komposition/Kunde.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komposition
+{
+    internal class Kunde
+    {
+        public int KundeId { get; set; }
+        public string Name { get; set; }
+
+        //Komposition zu Konto
+        public Konto[] Kontos = new Konto[5];
+    }
+}
