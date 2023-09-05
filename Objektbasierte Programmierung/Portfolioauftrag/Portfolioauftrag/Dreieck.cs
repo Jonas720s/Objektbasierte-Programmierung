d126c74e2ef8daf1a5774619785a2cdb0f0a45ef
Jonas720s <jonas.gemeinder@bluewin.ch>
2023-09-05 18:22:01 +0200

    Ungültige Dreiecke werden erkannt!

--- Objektbasierte Programmierung/Portfolioauftrag/Portfolioauftrag/Dreieck.cs
+++ Objektbasierte Programmierung/Portfolioauftrag/Portfolioauftrag/Dreieck.cs
@@ -14,9 +14,16 @@ namespace Portfolioauftrag
 
         public Dreieck(Linie Linie1, Linie Linie2, Linie Linie3)
         {
+        
             this.linie1 = Linie1;
             this.linie2 = Linie2;
             this.linie3 = Linie3;
+
+        }
+
+        public bool IstDreieckMoeglich()
+        {
+            return (linie1.Laenge + linie2.Laenge > linie3.Laenge) && (linie1.Laenge + linie3.Laenge > linie2.Laenge) && (linie2.Laenge + linie3.Laenge > linie1.Laenge);
         }
         public double BerechneUmfang()
         {
@@ -26,7 +33,8 @@ namespace Portfolioauftrag
         public double BerechneFlaeche()
         {
             double halbumfang = BerechneUmfang() / 2;
-            double flaeche = Math.Sqrt(halbumfang * (halbumfang - linie1.Laenge) * (halbumfang - linie2.Laenge) * (halbumfang - linie3.Laenge));
+            double v = Math.Sqrt(halbumfang * (halbumfang - linie1.Laenge) * (halbumfang - linie2.Laenge) * (halbumfang - linie3.Laenge));
+            double flaeche = v;
             return flaeche;
         }
     }
--- Objektbasierte Programmierung/Portfolioauftrag/Portfolioauftrag/Program.cs
+++ Objektbasierte Programmierung/Portfolioauftrag/Portfolioauftrag/Program.cs
@@ -4,50 +4,63 @@
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Wilkommen beim Formenkonstruktor! \nWelche Funktion möchten Sie aufrufen? \nF = Formenbehälter \nK = Kreis hinzufügen \nD = Dreieck hinzufügen \nR = Rechteck hinzufügen");
-            string antwort = Console.ReadLine().ToUpper();
-         
-            switch (antwort)
+            Console.WriteLine("Wilkommen beim Formenkonstruktor!");
+            bool formkonstruktor = true;
+            while (formkonstruktor == true)
             {
-                case "F":
-                    Console.WriteLine("...");
-                    break;
+                Console.WriteLine("\nWelche Funktion möchten Sie aufrufen? \nF = Formenbehälter \nK = Kreis hinzufügen \nD = Dreieck hinzufügen \nR = Rechteck hinzufügen");
+                string antwort = Console.ReadLine().ToUpper();
 
-                case "K":
-                    Console.Write("Geben Sie die Länger der Linie für den Kreis ein: ");
-                    Linie kreislinie = new Linie(Convert.ToDouble(Console.ReadLine()));
-                    Kreis kreis = new Kreis(kreislinie);
-                    Console.WriteLine($"Der Kreisumfang ist: {kreis.BerechneUmfang()}");
-                    Console.WriteLine($"Die Kreisfläche ist: {kreis.BerechneFlaeche()}");
-                    break;
+                switch (antwort)
+                {
+                    case "F":
+                        Console.WriteLine("...");
+                        break;
 
-                case "D":
-                    Console.Write("Geben Sie die Länge der Linien für das Dreieck ein: ");
-                    Linie dreieckslinie1 = new Linie(Convert.ToDouble(Console.ReadLine()));
-                    Console.Write("Geben Sie die Länge der Linien für das Dreieck ein: ");
-                    Linie dreieckslinie2 = new Linie(Convert.ToDouble(Console.ReadLine()));
-                    Console.Write("Geben Sie die Länge der Linien für das Dreieck ein: ");
-                    Linie dreieckslinie3 = new Linie(Convert.ToDouble(Console.ReadLine()));
-                    Dreieck dreieck = new Dreieck(dreieckslinie1, dreieckslinie2, dreieckslinie3);
-                    Console.WriteLine($"Der Umfang des Dreiecks beträgt: {dreieck.BerechneUmfang()}");
-                    Console.WriteLine($"Die Fläche des Dreiecks beträgt: {dreieck.BerechneFlaeche()}");
-                    break;
+                    case "K":
+                        Console.Write("Geben Sie die Länger der Linie für den Kreis ein: ");
+                        Linie kreislinie = new Linie(Convert.ToDouble(Console.ReadLine()));
+                        Kreis kreis = new Kreis(kreislinie);
+                        Console.WriteLine($"Der Kreisumfang ist: {kreis.BerechneUmfang()}");
+                        Console.WriteLine($"Die Kreisfläche ist: {kreis.BerechneFlaeche()}");
+                        break;
 
-                case "R":
-                    Console.Write("Geben Sie die Höhe des Rechtecks ein: ");
-                    Linie rechteckshöhe = new Linie(Convert.ToDouble(Console.ReadLine()));
-                    Console.Write("Geben Sie die Breite des Rechtecks ein: ");
-                    Linie rechtecksbreite = new Linie(Convert.ToDouble(Console.ReadLine()));
-                    Rechteck rechteck = new Rechteck(rechteckshöhe, rechtecksbreite);
-                    Console.WriteLine($" Der Umfang des Rechtecks beträgt: {rechteck.BerechneUmfang()}");
-                    Console.WriteLine($" Die Fläche des Rechtecks beträgt: {rechteck.BerechneFlaeche()}");
-                    break;
+                    case "D":
+                        Console.Write("Geben Sie die Länge der Linien für das Dreieck ein: ");
+                        Linie dreieckslinie1 = new Linie(Convert.ToDouble(Console.ReadLine()));
+                        Console.Write("Geben Sie die Länge der Linien für das Dreieck ein: ");
+                        Linie dreieckslinie2 = new Linie(Convert.ToDouble(Console.ReadLine()));
+                        Console.Write("Geben Sie die Länge der Linien für das Dreieck ein: ");
+                        Linie dreieckslinie3 = new Linie(Convert.ToDouble(Console.ReadLine()));
+                        Dreieck dreieck = new Dreieck(dreieckslinie1, dreieckslinie2, dreieckslinie3);
+                        if (dreieck.IstDreieckMoeglich())
+                        {
+                            Console.WriteLine($"Der Umfang des Dreiecks beträgt: {dreieck.BerechneUmfang()}");
+                            Console.WriteLine($"Die Fläche des Dreiecks beträgt: {dreieck.BerechneFlaeche()}");
+                        }                    
+                        else
+                        {
+                            Console.WriteLine("Diese Längen ergeben kein gültiges Dreieck!");
+                        }
+                        break;
 
-                default:
-                    Console.WriteLine("Diesen Befehl gibt es nicht!");
-                    break;
+                    case "R":
+                        Console.Write("Geben Sie die Höhe des Rechtecks ein: ");
+                        Linie rechteckshöhe = new Linie(Convert.ToDouble(Console.ReadLine()));
+                        Console.Write("Geben Sie die Breite des Rechtecks ein: ");
+                        Linie rechtecksbreite = new Linie(Convert.ToDouble(Console.ReadLine()));
+                        Rechteck rechteck = new Rechteck(rechteckshöhe, rechtecksbreite);
+                        Console.WriteLine($" Der Umfang des Rechtecks beträgt: {rechteck.BerechneUmfang()}");
+                        Console.WriteLine($" Die Fläche des Rechtecks beträgt: {rechteck.BerechneFlaeche()}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Diesen Befehl gibt es nicht!");
+                        formkonstruktor = false;
+                        break;
+                }
             }
+            
         }
     }
 }
